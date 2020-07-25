8f8b818f6718ea8fbc4fd3a0e0dbd33320fa2a4c
lechar0327 <953739446@qq.com>
2020-07-25 10:15:10 +0800

    去掉注册

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,25 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { insertUser, login } = require('../services/userServices');
 
-// /**
-//  * @api {post} http://10.36.147.178:3000/api/user/insertUser 注册
-//  * @apiDescription 注册
-//  * @apiName user-LoginRegister
-//  * @apiGroup user
-//  * @apiParam (body) {String} username 用户名
-//  * @apiParam (body) {String} pwd 密码
-//  *
-//  * @apiSuccess {Number} code 错误状态码.
-//  * @apiSuccess {String} msg  错误消息.
-//  * @apiSuccessExample {json} Success-Response:
-//  *  {
-//  *      "code" : "0",
-//  *      "msg" : "创建成功"
-//  * 
-//  *  }
-//  * @apiSampleRequest http://10.36.147.178:3000/api/user/insertUser
-//  * @apiVersion 1.0.0
-//  */
+
 router.post('/insertUser', insertUser);
 /**
  * @api {get} http://129.204.229.240:10086/api/user/login 登录即注册
